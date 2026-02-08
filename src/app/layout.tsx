cc12d21833fd1bb7a01ed18895ce55aa3a81bae2
Carlos Zabala <czabala847@gmail.com>
2026-02-07 22:42:06 -0500

    first commit

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,16 +1,8 @@
-import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
-import "./globals.css";
+import type { Metadata } from "next"
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
+import { geistMono, geistSans } from '@/config/fonts'
+import "./globals.css"
 
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
 
 export const metadata: Metadata = {
   title: "Create Next App",
